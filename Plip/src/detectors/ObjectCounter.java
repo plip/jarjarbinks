0c55c959da2cb4c00fa7eaab4547b0369dffd6f7
Martin Perez Invidio <mperez@cualit.com>
2013-11-16 17:22:40 -0200

    *Se usa MinAreaRect en el findCountours y distanceTransform

--- Plip/src/detectors/ObjectCounter.java
+++ Plip/src/detectors/ObjectCounter.java
@@ -9,6 +9,7 @@ import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Point;
+import org.opencv.core.RotatedRect;
 import org.opencv.core.Scalar;
 import org.opencv.highgui.Highgui;
 import org.opencv.imgproc.Imgproc;
@@ -19,12 +20,12 @@ public class ObjectCounter {
 
 	public void count() {
 		
-		Mat image = Highgui.imread(getClass().getResource("/54.jpg")
+		Mat image = Highgui.imread(getClass().getResource("/51.jpg")
 				.getPath());
 		
 		Mat imageWithEdges = edgeDetector(image);
 
-		Mat detectedObjects = findContours(imageWithEdges, 'o');
+		Mat detectedObjects = findContours(imageWithEdges);
 		
 		System.out.println(quantity);
 		/*Aplicar arcLength para saber si hay algun problema contornos muy grandes*/
@@ -33,7 +34,7 @@ public class ObjectCounter {
 		System.out.println(quantity);
 	}
 
-	public Mat findContours( Mat filteredImage , char type) {
+	public Mat findContours( Mat filteredImage) {
 		quantity = 0;
 		List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
 		MatOfPoint2f approx = new MatOfPoint2f();
@@ -43,34 +44,60 @@ public class ObjectCounter {
 		int count = 0;
 		/*Finds contours in the image and saves contours to contours object*/
 		Imgproc.findContours(filteredImage, contours, new Mat(),
-				Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+				Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_NONE);
 
-		Highgui.imwrite("contours"+type+".jpg", filteredImage);
+		Highgui.imwrite("contours.jpg", filteredImage);
 
 		for (int i = 0; i < contours.size(); i++) {
 
 			contours.get(i).convertTo(mMOP2f1, CvType.CV_32FC2);
-
-			Imgproc.approxPolyDP(mMOP2f1, approx,
-					Imgproc.arcLength(mMOP2f1, true) * 0.02, true);
-			approx.convertTo(mMOP, CvType.CV_32S);
+	
+//			Imgproc.approxPolyDP(mMOP2f1, approx,
+//					Imgproc.arcLength(mMOP2f1, true) * 0.02, true);
+//			
+//			approx.convertTo(mMOP, CvType.CV_32S);
+//			Point[] pointArray = approx.toArray();
+			RotatedRect rotated = Imgproc.minAreaRect(mMOP2f1);
+			Point[] vertices = new Point[4];
+			rotated.points(vertices);
+			MatOfPoint2f verticesMat = new MatOfPoint2f();
+			verticesMat.fromArray(vertices);
 			/*Draw contours between an estimated area, to avoid to big or to small objects*/
-			if (Imgproc.contourArea(approx) > 40000
-					&& Imgproc.contourArea(approx) < 6000000) {
+			if (Imgproc.contourArea(verticesMat) > 40000
+					&& Imgproc.contourArea(verticesMat) < 6000000) {
 
-				System.out.println(Imgproc.contourArea(approx));
+				System.out.println(Imgproc.contourArea(verticesMat));
 				Imgproc.drawContours(filteredImage, contours, i, s1, -1);
 				
-				Highgui.imwrite("contour" + i + type + ".jpg", filteredImage);
+				Highgui.imwrite("contour"+i+".jpg", filteredImage);
 
 				quantity++;
 			}
 		}
 		
-		Highgui.imwrite("contoursfilled"+type+".jpg", filteredImage);
+		Highgui.imwrite("contoursfilled.jpg", filteredImage);
 		
 		return filteredImage;
 	}
+	
+	public Mat findContoursAfterDistance( Mat filteredImage) {
+		quantity = 0;
+		List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+		MatOfPoint2f approx = new MatOfPoint2f();
+		MatOfPoint2f mMOP2f1 = new MatOfPoint2f();
+		MatOfPoint mMOP = new MatOfPoint();
+		Scalar s1 = new Scalar(255, 0, 0);
+		int count = 0;
+		/*Finds contours in the image and saves contours to contours object*/
+		Imgproc.findContours(filteredImage, contours, new Mat(),
+				Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_NONE);
+		quantity = contours.size();
+		Highgui.imwrite("contoursT.jpg", filteredImage);
+				Imgproc.drawContours(filteredImage, contours, -1, s1, -1);
+		Highgui.imwrite("contoursfilledT.jpg", filteredImage);		
+		return filteredImage;
+	}
+
 
 	public Mat edgeDetector( Mat image ) {
 		Mat medianBlur = new Mat();
@@ -110,7 +137,7 @@ public class ObjectCounter {
 		/*Distance transform to separate objects*/
 		
 		Imgproc.distanceTransform(detectedObjects, transform,
-				Imgproc.CV_DIST_L1, 3);
+				Imgproc.CV_DIST_L1, 0);
 		Highgui.imwrite("transform.jpg", transform);
 //		Mat transform = detectedObjects;
 		Core.normalize(transform, normalize, 0, 255, Core.NORM_MINMAX);
@@ -122,7 +149,7 @@ public class ObjectCounter {
 
 		thres.convertTo(thres, CvType.CV_8UC1);
 		
-		findContours(thres, 'd');
+		findContoursAfterDistance(thres);
 		
 		
 	}
